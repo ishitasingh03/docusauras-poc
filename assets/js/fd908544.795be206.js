37012b62dde4bf2488cef6e5c6f1fe8d8ac452c8
ishitasingh03 <ishitasingh03.vns@gmail.com>
2021-06-22 07:22:03 +0000

    Deploy website - based on f8cac3442246f25138ae05fe60cb4a6398ea99f7

--- assets/js/fd908544.795be206.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkexample=self.webpackChunkexample||[]).push([[214],{6353:function(e){"use strict";e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/docusauras-poc/blog/welcome"},{"title":"Hello","permalink":"/docusauras-poc/blog/hello-world"},{"title":"Hola","permalink":"/docusauras-poc/blog/hola"}]}')}}]);
